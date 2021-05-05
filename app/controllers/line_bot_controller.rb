784ea7cbe8d23398560660b41f5caf01ed82af6e
i-carrie <50307972+i-carrie@users.noreply.github.com>
2021-05-05 12:56:40 +0900

    Add error trap at controller

--- app/controllers/line_bot_controller.rb
+++ app/controllers/line_bot_controller.rb
@@ -13,10 +13,6 @@ class LineBotController < ApplicationController
         when Line::Bot::Event::Message
           case event.type
           when Line::Bot::Event::MessageType::Text
-            # message = {
-            #   type: 'text',
-            #   text: event.message['text']
-            # }
                 message = search_and_create_message(event.message['text'])
               client.reply_message(event['replyToken'], message)
           end
@@ -47,12 +43,16 @@ class LineBotController < ApplicationController
       response = http_client.get(url, query)
       response = JSON.parse(response.body)
 
-      text = ''
-      response['hotels'].each do |hotel|
-        text <<
-          hotel[0]['hotelBasicInfo']['hotelName'] + "\n" +
-          hotel[0]['hotelBasicInfo']['hotelInformationUrl'] + "\n" +
-          "\n"
+      if response.key?('error')
+        text = "この検索条件に該当する宿泊施設が見つかりませんでした。\n条件を変えて再検索してください。"
+      else
+        text = ''
+        response['hotels'].each do |hotel|
+          text <<
+            hotel[0]['hotelBasicInfo']['hotelName'] + "\n" +
+            hotel[0]['hotelBasicInfo']['hotelInformationUrl'] + "\n" +
+            "\n"
+        end
       end
 
       message = {
